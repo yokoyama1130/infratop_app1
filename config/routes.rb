6cc54edfbb251c3de9a21e41b63b07648129c334
EC2 Default User <ec2-user@ip-172-31-6-237.ap-northeast-1.compute.internal>
2025-03-28 09:24:25 +0000

    5章【編集機能を作ろう】

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create' 
-  get 'lists/edit'
   get 'top' => 'homes#top'
   get 'lists' => 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'list/:id/redit' => 'lists#edit', as: 'edit_list'
 end
+
