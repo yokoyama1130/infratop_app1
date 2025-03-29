1dff7972d0bf73f9679d3da127d1bbf533649ac8
EC2 Default User <ec2-user@ip-172-31-6-237.ap-northeast-1.compute.internal>
2025-03-29 11:37:01 +0000

    7章【削除機能を作ろう】

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,7 +30,7 @@ class ListsController < ApplicationController
   def destroy
     list = List.find(params[:id])
     list.destroy
-    redirect_to '/list'
+    redirect_to '/lists'
   end
 
   private
